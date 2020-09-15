13a6a0f2a43598c6e91fd5fc9842d38093bf31ec
Phillip <key.phillip@gmail.com>
2020-09-14 19:37:53 -0700

    Initial Commit

---
+++ What's PlayingTests/What_s_PlayingTests.swift
@@ -0,0 +1,34 @@
+//
+//  What_s_PlayingTests.swift
+//  What's PlayingTests
+//
+//  Created by Phillip Key on 9/14/20.
+//  Copyright © 2020 Phillip Key. All rights reserved.
+//
+
+import XCTest
+@testable import What_s_Playing
+
+class What_s_PlayingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
